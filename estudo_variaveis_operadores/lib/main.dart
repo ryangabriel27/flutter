22397d8f4b23d31ce0f891ad0894b314ed54e730
ryangabriel27 <ryanjulio56@gmail.com>
2024-02-15 19:51:26 -0300

    15/02/2023

---
+++ estudo_variaveis_operadores/lib/main.dart
@@ -0,0 +1,12 @@
+// Estudo variaveis
+void main() {
+  String nome = "João";
+  int idade = 25;
+  double altura = 1.75;
+  bool estudante = true;
+
+  print("Nome: $nome");
+  print("Idade: $idade anos");
+  print("Altura: $altura metros");
+  print("Estudante: $estudante");
+}
