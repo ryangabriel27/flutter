97f020fa6bec210d65b9fea948b01df3ffbacb6f
ryangabriel27 <ryanjulio56@gmail.com>
2024-03-11 19:50:55 -0300

    Terminar exercicio 8 e 6

--- exercicios_lista1/lib/Exercicio7.dart
+++ exercicios_lista1/lib/Exercicio7.dart
@@ -4,14 +4,50 @@ void main() {
   runApp(Exercicio7());
 }
 
-class Exercicio7 extends StatelessWidget{
+class Exercicio7 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Drawer"),
-        drawer:),
+        appBar: AppBar(
+          title: Text("Drawer"),
+          backgroundColor: const Color.fromARGB(255, 13, 47, 74),
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 13, 47, 74),
+                  
+                ),
+                child: Text(
+                  'Páginas',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Página inicial'),
+              ),
+              ListTile(
+                title: const Text('Favoritos'),
+              ),
+              ListTile(
+                title: const Text('Minha conta'),
+              ),
+              ListTile(
+                title: const Text('Configurações'),
+              ),
+            ],
+          ),
+        ),
       ),
-    )
+    );
   }
 }
---
+++ exercicios_lista1/lib/Exercicio8.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Exercicio8());
+}
+
+class Exercicio8 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double cardLargura = MediaQuery.of(context).size.width * 0.4;
+    double cardAltura = MediaQuery.of(context).size.height * 0.35;
+    double cardImgAltura = cardAltura * 0.6;
+    double cardImgLargura = cardLargura * 0.6;
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Página de produtos!"),
+          centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            Column(
+              children: [
+                Container(
+                  // 1° Card
+                  margin: EdgeInsets.all(10),
+                  width: cardLargura,
+                  height: cardAltura,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/produto1.png',
+                          width: cardImgLargura,
+                          height: cardImgAltura,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "Repelente OFF",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Center(
+                          child: Text(
+                            "Descrição do produto...",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  // 2° Card
+                  margin: EdgeInsets.all(10),
+                  width: cardLargura,
+                  height: cardAltura,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/produto2.jpg',
+                          width: cardImgLargura,
+                          height: cardImgAltura,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "Repelente OFF",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Center(
+                          child: Text(
+                            "Descrição do produto...",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  // 3° Card
+                  margin: EdgeInsets.all(10),
+                  width: cardLargura,
+                  height: cardAltura,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/produto3.png',
+                          width: cardImgLargura,
+                          height: cardImgAltura,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "Repelente OFF",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Center(
+                          child: Text(
+                            "Descrição do produto...",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  // 1° Card
+                  margin: EdgeInsets.all(10),
+                  width: cardLargura,
+                  height: cardAltura,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/produto4.png',
+                          width: cardImgLargura,
+                          height: cardImgAltura,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "Macarrão",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Center(
+                          child: Text(
+                            "Descrição do produto...",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  // 2° Card
+                  margin: EdgeInsets.all(10),
+                  width: cardLargura,
+                  height: cardAltura,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/produto2.jpg',
+                          width: cardImgLargura,
+                          height: cardImgAltura,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "Repelente OFF",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Center(
+                          child: Text(
+                            "Descrição do produto...",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  // 3° Card
+                  margin: EdgeInsets.all(10),
+                  width: cardLargura,
+                  height: cardAltura,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/produto3.png',
+                          width: cardImgLargura,
+                          height: cardImgAltura,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "Repelente OFF",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Center(
+                          child: Text(
+                            "Descrição do produto...",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
