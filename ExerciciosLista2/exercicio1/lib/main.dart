386cd8cacbd7907085aa9b29eae73e707ff602d3
ryangabriel27 <ryanjulio56@gmail.com>
2024-03-18 17:16:22 -0300

    Ex1 Feito

--- ExerciciosLista2/exercicio1/lib/main.dart
+++ ExerciciosLista2/exercicio1/lib/main.dart
@@ -8,27 +8,100 @@ class Exercicio1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Youtube"),
+          title: Text("Layout Calculadora"),
+          centerTitle: true,
         ),
-        body: Container(
-          padding: EdgeInsets.all(5.0),
-          child: Row(children: [
-            Column(  // Coluna para imagens
-              children: [
-                Image.asset('lib/assets/thumb.jpg')
-              ],
-            ),
-            Column(  // Coluna para titulos e descrições
-              children: [
-                Container(
-                  child: Text("TITULO"),
-                ),
-              ],
-            )
-          ],),
-        ),
+        body: Center(
+            child: Container(
+          width: 250,
+          height: 275,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: const Color.fromARGB(255, 181, 181, 181),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.green,
+                width: 200,
+                height: 100,
+              ),
+              SizedBox(height: 20), // Espaçamento entre o TextField e os botões
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("1"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("2"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("3"),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("4"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("5"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("6"),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("7"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("8"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("9"),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.add_call),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("0"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.cancel),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
